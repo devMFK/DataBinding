3a6fcd173335b06275cce8e9333347edf7aa3503
devMFK <mfurkan.keskin2@gmail.com>
2022-08-19 14:47:52 +0300

    Add project files.

---
+++ DataBinding/Info.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for Info.xaml
+    /// </summary>
+    public partial class Info : Window
+    {
+        public Info()
+        {
+            InitializeComponent();
+        }
+
+        private void TestingButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainWindow main = new MainWindow();
+            MessageBox.Show("Butona tıkladın!");
+            main.Show();
+            this.Close();
+        }
+    }
+}
---
+++ DataBinding/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public string name { get; set; } = "Bayram"; 
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = this;
+        }
+
+        private void HelpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Info infoWindow = new Info();
+            infoWindow.Show();
+            this.Close();
+
+        }
+
+        private void MainTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            MessageBox.Show("Klavyeye Bastın");
+        }
+    }
+}
